3a12a0c6d3e487446f3fc878915b7d24e32fe0e4
Isaac <const.isaac@gmail.com>
2023-10-22 09:29:48 -0400

    feat: update config to emit package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
+import { Box} from 'styled-system/jsx'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <Stack
-    ></Stack>
+    >
+      <Box></Box>
+    </Stack>
   )
 }
